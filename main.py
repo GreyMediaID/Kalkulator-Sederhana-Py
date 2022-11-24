9232c022d77dd6eadce98efe7275b344069ab91d
GreyMediaID <118594583+GreyMediaID@users.noreply.github.com>
2022-11-24 23:44:41 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+x = int(sys.argv[1])
+method = str(sys.argv[2])
+y = int(sys.argv[3])
+if method == "+":print(x + y)
+if method == "-":print(x - y)
+if method == "x":print(x * y)
+if method == "/":print(x / y)
